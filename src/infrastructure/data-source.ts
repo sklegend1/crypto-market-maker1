63d0a3f0a0beb021aea9de3f779eb4414973a0e1
sklegend1 <sut.sohrab@gmail.com>
2025-04-30 23:52:29 +0330

    feat : crypto market maker v0.2.0 -optimizing and validation added  -dev

--- src/infrastructure/data-source.ts
+++ src/infrastructure/data-source.ts
@@ -2,13 +2,14 @@ import { DataSource } from 'typeorm';
 import { User } from '../domain/entities/user';
 import { Order } from '../domain/entities/order';
 import { Trade } from '../domain/entities/trade';
+require('dotenv').config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5433,
   username: 'postgres',
-  password: 'Sohrab1158',
+  password: process.env.DBPASS!,
   database: 'users_api',
   entities: [User,Order,Trade],
   synchronize: true, // Auto-create tables (for development only)
